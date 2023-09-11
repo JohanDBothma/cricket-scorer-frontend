626ba8be8d74f564bf3cea3d8bacd887d82ef2c3
Johan <johandanielbothma@gmail.com>
2023-09-11 16:20:15 +0200

    Add basic TailwindUI header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form/Form';
 import Carousel from './components/Carousel';
+import Header from './components/Navigation/Header';
 
 function App() {
-  return (
-    <div className="">
-      <header className="">
-        <h1 className='text-3xl font-bold underline'>Home</h1>
-      </header>
-      {/* <Carousel /> */}
-    </div>
-  );
+    return (
+        <div className="">
+            <Header />
+            {/* <Carousel /> */}
+        </div>
+    );
 }
 
 export default App;
